505df971b842789003fee83277adbae5b478b7bd
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-05 10:17:14 +0530

    refactor: add step next funtionality and format App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import Step1_Personal from './Components/Step1_Personal'
 
 function App() {
-  
+  const [step, setStep] = useState(1)
 
-  
   return (
     <div className='w-full min-h-full bg-sky-200 flex justify-center'>
-      <main className='w-220 h-fit mt-16 p-6 bg-indigo-400'>
-        <h1 className='text-4xl'>Step 1- Personal Details</h1>
-        <hr className='my-4' />
-        <form className='flex flex-col gap-16 mt-24'>
-          <Step1_Personal />
-        </form>
+      <main
+        className={`w-220 ${
+          step === 1 ? 'h-212' : 'h-200'
+        } mt-16 p-6 bg-indigo-400`}
+      >
+        {step === 1 && <Step1_Personal setStep={setStep} />}
       </main>
     </div>
   )
